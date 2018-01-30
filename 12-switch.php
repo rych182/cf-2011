b315fb4f0104c0f02f5dbfd3ec6a319fe8e65e98
Ricardo Garrido <ricgarridoc@hotmail.com>
2018-01-29 18:35:14 -0600

    I made one exercise with switch in PHP5

---
+++ 12-switch.php
@@ -0,0 +1,14 @@
+<?php
+
+	$variable = "hola";
+	switch ($variable) {
+		case 1: echo "Vale uno";
+		break;
+		case 10: echo "Vale 10";
+		break;
+		case "hola": echo "Esta es una palabra";
+		break;
+		default: echo "No es ninguno";
+	}
+
+?>
