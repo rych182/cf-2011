5677fbf8b6dc39e84ac9033a9a30f22a39e7d1b4
Ricardo Garrido <ricgarridoc@hotmail.com>
2018-01-31 03:08:17 -0600

    I made 2 exercises with functions

---
+++ 13-funciones.php
@@ -0,0 +1,17 @@
+<?php
+/*EJEMPLO2
+	function rick($p1,$p2,$p3)
+	{
+		$suma = $p1+$p2+$p3; 
+		$multiplicacion = $suma * 2;
+		return $multiplicacion;
+	}
+	 echo rick(5,3,2);
+*/
+//eEJEMPLO 2
+	 function lol(){
+	 	$suma = 2+2;
+	 	return $suma;
+	 }
+	 echo lol();
+?>
