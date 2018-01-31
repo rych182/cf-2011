25107b0dcb1c33a34f935fba6cdafac30d56bc84
Ricardo Garrido <ricgarridoc@hotmail.com>
2018-01-31 04:11:49 -0600

    I made one form with PHP5 that can send information

---
+++ 14-formulario.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Formulario</title>
+</head>
+<body>
+	<form action="envio.php" method="post" name="frm">
+		<input type="text" name="nombre">
+		<input type="text" name="apellido">
+		<input type="submit" value="Click">
+	</form>
+</body>
+</html>
