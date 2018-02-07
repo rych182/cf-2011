f6246c83d99e7eef5fd2d6af1615f0e3eab99ebc
Ricardo Garrido <ricgarridoc@hotmail.com>
2018-02-07 05:36:04 -0600

    I learned how to create a register system

---
+++ 20-sistema-registro.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Sistema de registro</title>
+</head>
+<body>
+	<form action="registrar.php" method="post">
+		<table width="200">
+			<tr>
+				<td>Nombre:</td>
+				<td><input type="text" name="nombre"></td>
+			</tr>
+			<tr>
+				<td>Usuario</td>
+				<td><input type="text" name="user"></td>
+			</tr>
+			<tr>
+				<td>Password:</td>
+				<td><input type="password" name="pw"></td>
+			</tr>
+			<tr>
+				<td>Confirmar password:</td>
+				<td><input type="password" name="pw2"></td>
+			</tr>
+			<tr>
+				<td>E-mail:</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Registrar"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ registrar.php
@@ -0,0 +1,24 @@
+<?php
+include ('conexion1.php');
+	if (isset($_POST['nombre']) && !empty($_POST['nombre']) &&
+		isset($_POST['user']) && !empty($_POST['user']) &&
+		isset($_POST['pw']) && !empty($_POST['pw']) &&
+		isset($_POST['pw2']) && !empty($_POST['pw2']) &&
+		isset($_POST['email']) && !empty($_POST['email']) &&
+		($_POST['pw']) == ($_POST['pw2'])
+		) {
+		$con=mysql_connect($host,$user,$pw) or die("Problemas al conectar al server");
+
+		mysql_select_db($db,$con)or die("Problemas al conectar la base de datos");
+
+		mysql_query("INSERT INTO registro (nombre,user,pw,email) VALUES ('$_POST[nombre]','$_POST[user]','$_POST[pw]','$_POST[email]')",$con);
+		echo "Datos insertados<br>";
+		echo "Nombre: ".$_POST['nombre']."<br>";
+		echo "User: ".$_POST['user']."<br>";
+		echo "Password: ".$_POST['pw']."<br>";
+		echo "Email: ".$_POST['email']."<br>";
+	}else{
+		echo "Verifica si llenaste los campos o si los password coinciden";
+	}
+
+?>
